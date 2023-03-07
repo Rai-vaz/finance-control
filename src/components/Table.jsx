0260fdda3f529e525e6ac0dac7253e8c9e456b68
Rai-vaz <ray.vaz04@gmail.com>
2023-03-06 21:08:54 -0300

    apagar item

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,22 +7,28 @@ import '../css/Table.css'
 const Table = ({listItem}) => {
 
     const [dataList, setDataList] = useState([])
+    console.log(dataList.length)
 
+   
     useEffect(() => {
         setDataList([...dataList, listItem])
     },[listItem])
 
     function transformReal(coin){
-       if (coin != undefined) {
-            const toNumber = Number(coin)
-            const real = toNumber.toLocaleString('pt-br',{style:'currency', currency:'BRL'})
-            return real
-       }
+      if (coin != undefined) {
+        const toNumber = Number(coin) 
+        const real = toNumber.toLocaleString('pt-br',{style:'currency', currency:'BRL'})
+        return real
+      }
     }
 
-    function deleteItem(){
-        
+    function deleteItem(array, item){
+      const indexItem = array.indexOf(item)
+      console.log('indice apagado' + indexItem)
+      setDataList(() => array.splice(indexItem, 1))
     }
+
+   
 
   return (
     <table>
@@ -35,14 +41,14 @@ const Table = ({listItem}) => {
         </thead>
         <tbody>
             {
-                dataList.map((obj) => (
-                        <tr key={Math.random()}>
-                            <td>{obj.description}</td>
-                            <td>{transformReal(obj.value)}</td>
-                            <td>{obj.flow}<span>{obj.flow === 'Entrada' && <FaRegArrowAltCircleUp className='svg-entrada'/> }{obj.flow === 'Saída' && <FaRegArrowAltCircleDown className='svg-saída'/>}{obj.flow != undefined && <FaTrashAlt className='delete-icon' onClick={deleteItem}/>}</span></td>
-                        </tr>
-                    )
+              dataList.map((obj) => (
+                  <tr key={Math.random()}>
+                      <td>{obj.description}</td>
+                      <td>{transformReal(obj.value)}</td>
+                      <td>{obj.flow}<span>{obj.flow === 'Entrada' && <FaRegArrowAltCircleUp className='svg-entrada'/> }{obj.flow === 'Saída' && <FaRegArrowAltCircleDown className='svg-saída'/>}{obj.flow != undefined && <FaTrashAlt className='delete-icon' onClick={() => deleteItem(dataList, obj)}/>}</span></td>
+                  </tr>
                 )
+              )
             }
         </tbody>
       </table>
